f484134fd559d7d62799ca67ec7c3cce2860f0e9
Ndumiso <ndumisovee@gmail.com>
2024-06-18 23:23:51 +0200

    Add basic_operstions.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,46 @@
+int number1 = 10
+int number 2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print (int number1 = 10
+int number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print (f"Addition of {number1} and {number2} is {sum}")
+print (f"Subtraction of {number1} and {number2} is {difference}")
+print (f"Multiplication of {number1} and {number2} is {product}")
+
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+basic_operations.py[+] [unix] (01:59 01/01/1970)                                                           6,8 All
+-- INSERT --
+
