c8a840e78a881cdfde71de7f46c5fe02af63cd88
Ndumiso <ndumisovee@gmail.com>
2024-07-13 22:26:28 +0200

    Added bank_acccount and main_0 files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        print(f"Deposited: ${amount}")
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+        else:
+            print("Insufficient funds")
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
