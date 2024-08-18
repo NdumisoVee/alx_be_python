84e3c535b3790ee205e78659c15e65f3c5571873
Ndumiso <ndumisovee@gmail.com>
2024-08-18 03:55:50 +0200

    file updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -16,7 +16,7 @@ class Book:
 
 
 # Derived Class - EBook
-class EBook:
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self._file_size = file_size
@@ -26,11 +26,11 @@ class EBook:
         return self._file_size
 
     def __str__(self):
-        return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
+        return f"E{super().__str__()}, File Size: {self.file_size}KB"
 
 
-# Derived Class - PrintBook
-class PrintBook:
+k
+class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self._page_count = page_count
@@ -40,7 +40,7 @@ class PrintBook:
         return self._page_count
 
     def __str__(self):
-        return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
+        return f"Print: {super().__str__()}, Page Count: {self.page_count}"
 
 
 class Library:
