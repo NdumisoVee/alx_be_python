f92ebcdbad824fc122164dbb582b712aa347586e
Ndumiso <ndumisovee@gmail.com>
2024-08-18 03:48:11 +0200

    file updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,38 +1,56 @@
-
 class Book:
     def __init__(self, title, author):
-        self.title = title
-        self.author = author
+        self._title = title
+        self._author = author
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
+# Derived Class - EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size
+        self._file_size = file_size
+
+    @property
+    def file_size(self):
+        return self._file_size
 
     def __str__(self):
-        return f"EBook: {super().__str__()}, file size: {self.file_size}KB]"
+        return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
 
+# Derived Class - PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count = None
+        self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
 
     def __str__(self):
-        return f"Printbook: {super().__str__()}, Page Count: {self.page_count}"
+        return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
-        self.books = []
+        self._books = []
 
-    def add_book(self, book):
-        self.books.append(book)
+    def add_book(self, book: Book):
+        if isinstance(book, Book):
+            self._books.append(book)
+        else:
+            raise TypeError("Only instances of Book or its subclasses can be added to the library.")
 
     def list_books(self):
-        if not self.books:
-            print("No books in the library.")
-        else:
-            for book in self.books:
-                print(book)
+        for book in self._books:
+            print(book)
